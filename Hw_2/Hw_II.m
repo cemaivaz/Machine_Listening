cfc8abe5fb405d12af0b6830408bcea530257562
cemaivaz <cemrifkiaydin@gmail.com>
2015-04-01 01:08:42 +0300

    A little change performed

--- Hw_2/Hw_II.m
+++ Hw_2/Hw_II.m
@@ -26,55 +26,6 @@ end
 
 
 
-figure(8);
-% get a section of vowel
-[x,fs]=wavread('i.wav');
-ms1=fs/1000;                 % maximum speech Fx at 1000Hz
-ms20=fs/50;                  % minimum speech Fx at 50Hz
-
-% ms1=1;
-% ms20=length(x);
-%
-% plot waveform
-t=(0:length(x)-1)/fs;        % times of sampling instants
-subplot(3,1,1);
-plot(t,x);
-legend('Waveform');
-xlabel('Time (s)');
-ylabel('Amplitude');
-%
-% do fourier transform of windowed signal
-ms1=fs/1000;                 % maximum speech Fx at 1000Hz
-ms20=fs/50;                  % minimum speech Fx at 50Hz
-
-Y=fft(x.*hamming(length(x)));
-%
-% plot spectrum of bottom 5000Hz
-hz5000=5000*length(Y)/fs;
-f=(0:hz5000)*fs/length(Y);
-subplot(3,1,2);
-plot(f,20*log10(abs(Y(1:length(f)))+eps));
-legend('Spectrum');
-xlabel('Frequency (Hz)');
-ylabel('Magnitude (dB)');
-%
-% cepstrum is DFT of log spectrum
-C=fft(log(abs(Y)+eps));
-%
-% plot between 1ms (=1000Hz) and 20ms (=50Hz)
-q=(ms1:ms20)/fs;
-subplot(3,1,3);
-plot(q,abs(C(ms1:ms20)));
-legend('Cepstrum');
-xlabel('Quefrency (s)');
-ylabel('Amplitude');
-
-% figure(8);
-% c = cceps(s2);
-%
-% plot(t,c)
-% xlabel('Time (s)')
-% title('Complex cepstrum')
 
 %%
 
@@ -227,9 +178,9 @@ NFFT = 128;
 
 tSpl = 1/Fs;
 
-stInd = round(0.2/tSpl)
+stInd = round(0.2/tSpl);
 
-endInd = round(0.8/tSpl)
+endInd = round(0.8/tSpl);
 
 %vals = y(stInd:endInd);
 
@@ -237,7 +188,8 @@ vals = y;
 
 x1 = vals.*hamming(length(vals));
 
-preemph = [1/4 1/4 1/4 1/4];
+%Degisik preemph'ler icin dene
+preemph = [1];
 
 x1 = filter(1,preemph,x1);
 
