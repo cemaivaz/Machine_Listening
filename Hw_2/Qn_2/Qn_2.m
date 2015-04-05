a332abd5d8c3a4d2f332e92c0b8c923b263b5e91
cemaivaz <cemrifkiaydin@gmail.com>
2015-04-05 16:03:07 +0300

    Question 2 variable names changed

--- Hw_2/Qn_2/Qn_2.m
+++ Hw_2/Qn_2/Qn_2.m
@@ -32,38 +32,21 @@ for u = 1:length(allData)
     p = [];
     [data fs] = wavread(fileMv);
     
-    % YIN2: a simple implementation of the yin period-estimation algorithm
-    %
-    %  yin2(data) : plot the period, power, and aperiodicity as a function of time
-    %
-    %  r=yin2(data,p): use parameters in p, return result in r:
-    %
-    %    yin.prd: period
-    %    yin.ap: aperiodicity measure
-    %
-    %    maxprd: samples, maximum of search range [default 100]
-    %    minprd: samples, minimum of search range [default 2]
-    %    wsize: samples, window size [default maxprd]
-    %    skip: samples, frame period [default wsize]
-    %    thre_: thre_old for period minimum [default: 0.1]
-    %    smooth: samples, size of low-pass smoothing window [default: minprd/2]
-    
-    
-    % defaults
+    %My implementation for YIN algorithm is given below. I utilized
+    %interpolating parabolic equation for it.
     
     dataSize = length(data);
     
-    thre_ = 0.11;
+    thre_ = 0.11; %yin threshold
     range_ = 256;
     
     windLen = range_;
-    skip = windLen;
+    skip = windLen; %It denotes the frame period
     
     
-    frameNo = floor((dataSize - range_ - windLen) / skip);
+    frameNo = floor((dataSize - range_ - windLen) / skip); %The number of frames
     
     
-    aperiod = ones(1, frameNo);
     period_ = ones(1, frameNo);
     p_ = ones(1, frameNo); %pwr
     
@@ -75,41 +58,49 @@ for u = 1:length(allData)
 
     while k <= frameNo
         
-        st_ = k * skip - skip; % offset of frame
+        %The frame offset is defined below
+        st_ = k * skip - skip; 
         xSub = data(st_ + 1:st_ + windLen,:);
         repAmount = range_ + 1;
-        dist_ = .5 * mean(power(xSub - repmat(xSub(:,1),1,repAmount), 2));     % squared difference function
-        cumM_ = dist_(2:length(dist_)) ./ (cumsum(dist_(2:length(dist_))) ./ (1:(repAmount - 1)));   % cumulative mean - normalized
+        %The difference is stated below (mean square)
+        dist_ = .5 * mean(power(xSub - repmat(xSub(:,1),1,repAmount), 2)); 
+        %cumM_ = cumulative mean
+        cumM_ = dist_(2:length(dist_)) ./ (cumsum(dist_(2:length(dist_))) ./ (1:(repAmount - 1)));
         
-        % parabolic interpolation of all triplets to refine local minima
-        positionsMin=1:length(cumM_);    % nominal position of each sample
+        % Interpolation getting implemented below
+        positionsMin=1:length(cumM_);
         
         firstX = cumM_(1:length(cumM_)-2);
         secX = cumM_(2:length(cumM_)-1);
         thirdX = cumM_(3:length(cumM_));
+        %Parabolic coefficients in accordance with the algorithm I developed
+        %are given below
         a_ = .5 * (firstX + thirdX - 2. * secX);
         b_ = .5 * (thirdX - firstX);
-        sh_ = -b_ ./ a_ * .5;        % offset of interpolated minimum re current sample
-        output = secX - b_ .^2 ./ a_ .* 0.25;     % value of interpolated minimum
+        sh_ = -b_ ./ a_ * .5;
+        output = secX - b_ .^2 ./ a_ .* 0.25;
         
-        % replace all local minima by their interpolated value,
+        
         ind_ = find(secX<firstX & secX<thirdX) + 1.;
         cumM_(ind_) = output(-1. + ind_);
         positionsMin(ind_) = positionsMin(-1. + ind_) + sh_(-1. + ind_);
         
-        % find index of first min below thre_old
+        %Minimum values are getting extracted below
         
         a_ = zeros(length(cumM_));
         for o_ = 1:length(cumM_)
             a_(o_) = (cumM_(o_) < thre_);
         end
         
+        %Global minimum or local minimum are getting taken into account
+        %below
         if length(find(a_)) == 0
-            [minVal, indMin] = min(cumM_); % none below thre_old, take global min instead
+            
+            [minVal, indMin] = min(cumM_);
         else
             bTmp = find(a_);
             bTmp = bTmp(1);
-            b_ = min(bTmp); % left edge
+            b_ = min(bTmp);
             c_ = min(b_*2., length(a_));
             [minVal, indMin] = min(cumM_(b_:(c_-1)));
             indMin = -1 + b_ + indMin;
@@ -118,18 +109,20 @@ for u = 1:length(allData)
         period_ = positionsMin(indMin);
         period_ = period_ + 1;
         
+        %Parabola values and coefficients being used for extracting the
+        %period value
         if dist_(indMin) < dist_(indMin+1)
             if period_ < length(cumM_)
                 if dist_(indMin) < dist_(indMin-1)
                     if period_ > 2 
-                        % refine by parabolic interpolation of raw difference function
+                        
                         firstX = dist_(-1. + period_);
                         secX = dist_(period_);
                         thirdX = dist_(1. + period_);
                         a_= .5 * (firstX + thirdX - 2. * secX);
                         b_= .5 * (thirdX - firstX);
-                        sh_= -b_ ./ a_ ./ 2;        % offset of interpolated minimum re current sample
-                        output = secX - b_ .^ 2 ./ a_ ./ 4;     % value of interpolated minimum
+                        sh_= -b_ ./ a_ ./ 2; 
+                        output = secX - b_ .^ 2 ./ a_ ./ 4; 
                         period_ = period_+sh_;
                         period_ = period_ - 1.;
                     end
@@ -137,25 +130,14 @@ for u = 1:length(allData)
             end
         end
         
-        % aperiodicity
-        fr_ = period_;
-        fr_ = fr_ - floor(period_);
-        if fr_ == .0
-            xRes = xSub(:,period_);
-        else
-            xRes = xSub(:, floor(period_ + 1)) .* (1 - fr_);
-            xRes = xRes + xSub(:, floor(period_ + 1) + 1) .* fr_;% linear interpolation
-        end
-        p_ = .5 * (mean(power(xSub(:, 1), 2)) + mean(power(xRes, 2))); % average power over fixed and shifted windows
-        result = .5 * mean(power(((xSub(:, 1) - xRes)), 2));
-       
-        
         yin.period_(k) = period_;
         
         k = k + 1;
     end
 
+    
     fileMv(8:end)
+    %Fundamental frequencies getting printed below
     fs ./ yin.period_
     
 end
